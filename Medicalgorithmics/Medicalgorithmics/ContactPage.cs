d765d945f421d85906faa4c252ac7b6b17e8ebf2
Aleksander Parol <alekparol92@gmail.com>
2019-11-05 23:24:36 +0100

    Create ContactPage.cs
    Creating C# class which represents "Kontakt" page according to Page Object Model.

---
+++ Medicalgorithmics/Medicalgorithmics/ContactPage.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medicalgorithmics
+{
+    class ContactPage
+    {
+
+        public string URLAddress = "https://www.medicalgorithmics.pl/kontakt";
+
+        public IWebElement mediaPackButton;
+
+        public void MediaPackClick()
+        {
+
+        }
+
+        public void ScrollToTheMediaPack()
+        {
+
+        }
+
+        public ContactPage(IWebDriver driver)
+        {
+
+        }
+
+    }
+}
