b764dfd2cd21b74bcc20e1b0e383c5fd84ddabe4
Aleksander Parol <alekparol92@gmail.com>
2019-11-05 22:45:40 +0100

    Update MainPage.cs

--- Medicalgorithmics/Medicalgorithmics/MainPage.cs
+++ Medicalgorithmics/Medicalgorithmics/MainPage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,25 @@ namespace Medicalgorithmics
 {
     class MainPage
     {
+
+        public string URLAddress = "https://www.medicalgorithmics.pl/";
+
+        public IWebElement kontaktButton;
+
+        void KontaktClick()
+        {
+            kontaktButton.Click();
+        }
+
+        public MainPage(IWebDriver driver)
+        {
+
+            WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 10));
+            wait.Until(Driver => driver.FindElement(By.Id("mega-menu-item-29")));
+
+            kontaktButton = driver.FindElement(By.Id("mega-menu-item-29"));
+        
+        }
+
     }
 }
