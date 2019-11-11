1099ad2124e36e58f940c6a41973f5613e882544
Aleksander Parol <alekparol92@gmail.com>
2019-11-11 15:30:28 +0100

    Update MainPage.cs
    Adding fileds, public methods and a construtor to the MainPage class.

--- Medicalgorithmics/Medicalgorithmics/MainPage.cs
+++ Medicalgorithmics/Medicalgorithmics/MainPage.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
@@ -9,23 +10,86 @@ namespace Medicalgorithmics
     class MainPage
     {
 
-        public string URLAddress = "https://www.medicalgorithmics.pl/";
+        public string expectedURLAddress = "https://www.medicalgorithmics.pl/";
+        private Boolean loadingError = false;
 
-        public IWebElement kontaktButton;
+        protected IWebElement contactButton;
+        protected IWebElement acceptCookiesButton;
+        protected IWebElement searchEngineButton;
+        protected IWebElement searchEngineTextField;
+        protected IWebElement searchEngingeSubmit;
 
-        void KontaktClick()
+        public void ContactClick()
         {
-            kontaktButton.Click();
+            contactButton.Click();
+        }
+
+        public string GetContactColor()
+        {
+            return contactButton.GetCssValue("color");
+        }
+
+        public void ContactGoTo(IWebDriver driver)
+        {
+            Actions builder = new Actions(driver);
+            builder.MoveToElement(contactButton).Perform();
+        }
+
+        public void AcceptCookies()
+        {
+            acceptCookiesButton.Click();
+        }
+
+        public void SearchButtonClick()
+        {
+            searchEngineButton.Click();
+        }
+
+        public void SearchFieldFill(string search)
+        {
+            searchEngineTextField.SendKeys(search);
+            searchEngineTextField.Submit();
+        }
+
+        public Boolean GetLoadingError()
+        {
+            return loadingError;
+        }
+
+        public Boolean IsSubmitVisible()
+        {
+            string ariaHidden = searchEngingeSubmit.GetAttribute("aria-hidden");
+
+            if(ariaHidden == "true")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public MainPage(IWebDriver driver)
         {
 
-            WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 10));
-            wait.Until(Driver => driver.FindElement(By.Id("mega-menu-item-29")));
+            if(driver.Url == expectedURLAddress)
+            {
+                WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 10));
+                wait.Until(Driver => driver.FindElement(By.Id("mega-menu-item-29")));
 
-            kontaktButton = driver.FindElement(By.Id("mega-menu-item-29"));
-        
+                contactButton = driver.FindElement(By.CssSelector("#mega-menu-item-29 > a"));
+                acceptCookiesButton = driver.FindElement(By.Id("cn-accept-cookie"));
+
+                searchEngineButton = driver.FindElement(By.XPath("//a[contains(@class, 'search_button')]"));
+                searchEngineTextField = driver.FindElement(By.Name("s"));
+                searchEngingeSubmit = driver.FindElement(By.XPath("//*[contains(@class, 'qode_icon_font_elegant arrow_right qode_icon_element')]"));
+            }
+            else
+            {
+                loadingError = true;
+            }
+
         }
 
     }
