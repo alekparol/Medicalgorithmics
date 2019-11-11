99bb0bb83c9784bd59f329cd897719560b8a2ba4
Aleksander Parol <alekparol92@gmail.com>
2019-11-11 16:14:07 +0100

    Update ContactPage.cs
    Adding fields, methods and a constructor for ContactPage.cs.

--- Medicalgorithmics/Medicalgorithmics/ContactPage.cs
+++ Medicalgorithmics/Medicalgorithmics/ContactPage.cs
@@ -1,4 +1,6 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,23 +10,41 @@ namespace Medicalgorithmics
     class ContactPage
     {
 
-        public string URLAddress = "https://www.medicalgorithmics.pl/kontakt";
+        public string expectedURLAddress = "https://www.medicalgorithmics.pl/kontakt";
+        private Boolean loadingError = false;
 
-        public IWebElement mediaPackButton;
+        protected IWebElement mediaPackButton;
 
         public void MediaPackClick()
         {
-
+            mediaPackButton.Click();
         }
 
-        public void ScrollToTheMediaPack()
+        public void ScrollToTheMediaPack(IWebDriver driver)
         {
+            Actions builder = new Actions(driver);
+            builder.MoveToElement(mediaPackButton).Perform();
+        }
 
+        public Boolean GetLoadingError()
+        {
+            return loadingError;
         }
 
         public ContactPage(IWebDriver driver)
         {
+            if (driver.Url == expectedURLAddress)
+            {
+                WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 10));
+                wait.Until(Driver => driver.FindElement(By.XPath("//a[text()='Media pack']")));
 
+                mediaPackButton = driver.FindElement(By.XPath("//a[text()='Media pack']"));
+               
+            }
+            else
+            {
+                loadingError = true;
+            }
         }
 
     }
