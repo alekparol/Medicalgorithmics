3166e3977c7e29b41d6c1f662acd18c1148dc684
Aleksander Parol <alekparol92@gmail.com>
2019-11-11 17:49:17 +0100

    Create MediaPackPage
    Creating MediaPackPage with class fields, methods and constructior.

---
+++ Medicalgorithmics/Medicalgorithmics/MediaPackPage.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medicalgorithmics
+{
+    class MediaPackPage
+    {
+
+        public string expectedURLAddress = "https://www.medicalgorithmics.pl/media-pack";
+        private Boolean loadingError = false;
+
+        protected IWebElement logotypyButton;
+
+        public void DonwloadLogotypy()
+        {
+            logotypyButton.Click();
+        }
+
+        public Boolean GetLoadingError()
+        {
+            return loadingError;
+        }
+
+        public MediaPackPage(IWebDriver driver)
+        {
+            if (driver.Url == expectedURLAddress)
+            {
+                WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 10));
+                wait.Until(Driver => driver.FindElement(By.XPath("//strong[text()='Logotypy']")));
+
+                logotypyButton = driver.FindElement(By.XPath("//strong[text()='Logotypy']"));
+
+            }
+            else
+            {
+                loadingError = true;
+            }
+        }
+
+    }
+}
